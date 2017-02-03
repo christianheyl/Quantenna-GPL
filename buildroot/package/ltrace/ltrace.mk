09c53d6b84f63fb5563b4d29883bbccea67b955e
christianheyl <Chr.Heyl@googlemail.com>
2017-02-03 11:21:35 +0100

    updated ltrace package to 0.7.3. WARNING: doesn't compile at the moment!

--- buildroot/package/ltrace/ltrace.mk
+++ buildroot/package/ltrace/ltrace.mk
@@ -3,48 +3,49 @@
 # ltrace
 #
 #############################################################
-LTRACE_SOURCE=ltrace_0.3.36.orig.tar.gz
-LTRACE_SOURCE2=ltrace_0.3.36-2.diff.gz
-LTRACE_SITE=http://ftp.debian.org/debian/pool/main/l/ltrace
-LTRACE_DIR=$(BUILD_DIR)/ltrace-0.3.36
-LTRACE_BINARY=ltrace
-LTRACE_TARGET_BINARY=usr/bin/ltrace
+LTRACE_VERSION:=0.7.3
+LTRACE_SOURCE:=ltrace_$(LTRACE_VERSION).orig.tar.bz2
+LTRACE_SITE:=http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/l/ltrace
+LTRACE_DIR:=$(BUILD_DIR)/ltrace-$(LTRACE_VERSION)
+LTRACE_CAT:=$(BZCAT)
+LTRACE_BINARY:=ltrace
+LTRACE_TARGET_BINARY:=usr/bin/ltrace
 
-LTRACE_ARCH:=$(ARCH)
-ifeq ("$(strip $(ARCH))","armeb")
-LTRACE_ARCH:=arm
-endif
+#LTRACE_ARCH:=$(ARCH)
+#ifeq ("$(strip $(ARCH))","armeb")
+#LTRACE_ARCH:=arm
+#endif
 
 $(DL_DIR)/$(LTRACE_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LTRACE_SITE)/$(LTRACE_SOURCE)
 
-$(DL_DIR)/$(LTRACE_SOURCE2):
-	$(WGET) -P $(DL_DIR) $(LTRACE_SITE)/$(LTRACE_SOURCE2)
 
-$(LTRACE_DIR)/.source: $(DL_DIR)/$(LTRACE_SOURCE) $(DL_DIR)/$(LTRACE_SOURCE2)
-	$(ZCAT) $(DL_DIR)/$(LTRACE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(ZCAT) $(DL_DIR)/$(LTRACE_SOURCE2) | patch -p1 -d $(LTRACE_DIR)
-	touch $(LTRACE_DIR)/.source
+$(LTRACE_DIR)/.unpacked: $(DL_DIR)/$(LTRACE_SOURCE) $(DL_DIR)/$(LTRACE_SOURCE2)
+	$(LTRACE_CAT) $(DL_DIR)/$(LTRACE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+	toolchain/patch-kernel.sh $(LTRACE_DIR) package/ltrace/ ltrace\*.patch
+	touch $(LTRACE_DIR)/.unpacked
 
-$(LTRACE_DIR)/.configured: $(LTRACE_DIR)/.source
+$(LTRACE_DIR)/.configured: $(LTRACE_DIR)/.unpacked
 	(cd $(LTRACE_DIR); \
 		$(TARGET_CONFIGURE_OPTS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)" \
 		./configure \
 		--target=$(GNU_TARGET_NAME) \
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
 		--prefix=/usr \
+		--includedir=/include \
 		--sysconfdir=/etc \
+		--disable-static \
 	);
 	touch $(LTRACE_DIR)/.configured;
 
 $(LTRACE_DIR)/$(LTRACE_BINARY): $(LTRACE_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_CROSS)ld ARCH=$(LTRACE_ARCH) \
-		-C $(LTRACE_DIR)
+	$(MAKE) CC=$(TARGET_CC) -C $(LTRACE_DIR)
 
 $(TARGET_DIR)/$(LTRACE_TARGET_BINARY): $(LTRACE_DIR)/$(LTRACE_BINARY)
-	$(MAKE) DESTDIR=$(TARGET_DIR) ARCH=$(LTRACE_ARCH) \
-		-C $(LTRACE_DIR) install
+	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(LTRACE_DIR) install
 	rm -Rf $(TARGET_DIR)/usr/man
 
 ltrace: uclibc libelf $(TARGET_DIR)/$(LTRACE_TARGET_BINARY)
