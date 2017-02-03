e666f8ca79e8ca4035e615abcc7b334b33fe2cad
christianheyl <Chr.Heyl@googlemail.com>
2017-02-03 11:04:31 +0100

    fixed dl URL for pppd-2.4.4 package

--- buildroot/package/pppd/pppd.mk
+++ buildroot/package/pppd/pppd.mk
@@ -5,7 +5,9 @@
 #############################################################
 PPPD_VERSION:=2.4.4
 PPPD_SOURCE:=ppp-$(PPPD_VERSION).tar.gz
-PPPD_SITE:=ftp://ftp.samba.org/pub/ppp
+#PPPD_SITE:=ftp://ftp.samba.org/pub/ppp
+#new dl site
+PPPD_SITE:=https://download.samba.org/pub/ppp
 PPPD_DIR:=$(BUILD_DIR)/ppp-$(PPPD_VERSION)
 PPPD_CAT:=$(ZCAT)
 PPPD_BINARY:=pppd/pppd
