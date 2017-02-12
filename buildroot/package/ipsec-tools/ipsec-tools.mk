21e67fba6d5a82733c896b4feae09e885b7c6913
christianheyl <Chr.Heyl@googlemail.com>
2017-02-12 14:24:58 +0100

    update ipsec-tools to version 0.7.2, adding needed patches

--- buildroot/package/ipsec-tools/ipsec-tools.mk
+++ buildroot/package/ipsec-tools/ipsec-tools.mk
@@ -4,10 +4,10 @@
 #
 #############################################################
 
-IPSEC_TOOLS_VER:=0.6.6
-IPSEC_TOOLS_SOURCE:=ipsec-tools-$(IPSEC_TOOLS_VER).tar.bz2
+IPSEC_TOOLS_VERSION:=0.7.2
+IPSEC_TOOLS_SOURCE:=ipsec-tools-$(IPSEC_TOOLS_VERSION).tar.bz2
 IPSEC_TOOLS_CAT:=$(BZCAT)
-IPSEC_TOOLS_DIR:=$(BUILD_DIR)/ipsec-tools-$(IPSEC_TOOLS_VER)
+IPSEC_TOOLS_DIR:=$(BUILD_DIR)/ipsec-tools-$(IPSEC_TOOLS_VERSION)
 
 IPSEC_TOOLS_BINARY_SETKEY:=src/setkey/setkey
 IPSEC_TOOLS_BINARY_RACOON:=src/racoon/racoon
@@ -16,64 +16,75 @@ IPSEC_TOOLS_BINARY_RACOONCTL:=src/racoon/racoonctl
 IPSEC_TOOLS_TARGET_BINARY_SETKEY:=usr/sbin/setkey
 IPSEC_TOOLS_TARGET_BINARY_RACOON:=usr/sbin/racoon
 IPSEC_TOOLS_TARGET_BINARY_RACOONCTL:=usr/sbin/racoonctl
-IPSEC_TOOLS_SITE=http://$(BR2_SOURCEFORGE_MIRROR).dl.sourceforge.net/sourceforge/ipsec-tools/
+#IPSEC_TOOLS_SITE=http://ftp.sunet.se/pub/NetBSD/misc/ipsec-tools/0.7/
+#new d/l site
+IPSEC_TOOLS_SITE=ftp://ftp.netbsd.org/pub/NetBSD/misc/ipsec-tools/0.7/
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--enable-adminport
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-adminport
 else
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-adminport
+IPSEC_TOOLS_CONFIG_FLAGS+= --disable-adminport
 endif
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_NATT)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--enable-natt
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_NATT), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-natt
 else
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-natt
+IPSEC_TOOLS_CONFIG_FLAGS+= --disable-natt
 endif
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_FRAG)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--enable-frag
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_FRAG), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-frag
 else
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-frag
+IPSEC_TOOLS_CONFIG_FLAGS+= --disable-frag
 endif
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_STATS)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--enable-stats
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_STATS), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-stats
 else
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-stats
+IPSEC_TOOLS_CONFIG_FLAGS+= --disable-stats
 endif
 
-# At first check, if uClibc supports IPv6
-ifeq ($(shell grep -qs '__UCLIBC_HAS_IPV6__ 1' \
-	$(STAGING_DIR)/include/bits/uClibc_config.h && echo IPV6), IPV6)
+ifeq ($(BR2_INET_IPV6),y)
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_IPV6)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--enable-ipv6
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_IPV6), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-ipv6
 else
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-ipv6
+IPSEC_TOOLS_CONFIG_FLAGS+= $(DISABLE_IPV6)
 endif
 
 else # ignore user's choice if it doesn't
-IPSEC_TOOLS_CONFIG_FLAGS+=	--disable-ipv6
+IPSEC_TOOLS_CONFIG_FLAGS+= $(DISABLE_IPV6)
 endif
 
-ifneq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_READLINE)), y)
-IPSEC_TOOLS_CONFIG_FLAGS+=	--without-readline
+ifneq ($(BR2_PACKAGE_IPSEC_TOOLS_READLINE), y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --without-readline
+endif
+
+ifeq ($(BR2_PACKAGE_IPSEC_SECCTX_DISABLE),y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-security-context=no
+endif
+ifeq ($(BR2_PACKAGE_IPSEC_SECCTX_ENABLE),y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-security-context=yes
+endif
+ifeq ($(BR2_PACKAGE_IPSEC_SECCTX_KERNEL),y)
+IPSEC_TOOLS_CONFIG_FLAGS+= --enable-security-context=kernel
 endif
 
 $(DL_DIR)/$(IPSEC_TOOLS_SOURCE):
 	$(WGET) -P $(DL_DIR) $(IPSEC_TOOLS_SITE)/$(IPSEC_TOOLS_SOURCE)
 
-
-$(IPSEC_TOOLS_DIR)/.source: $(DL_DIR)/$(IPSEC_TOOLS_SOURCE)
+$(IPSEC_TOOLS_DIR)/.patched: $(DL_DIR)/$(IPSEC_TOOLS_SOURCE)
 	$(IPSEC_TOOLS_CAT) $(DL_DIR)/$(IPSEC_TOOLS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(IPSEC_TOOLS_DIR) package/ipsec-tools ipsec-tools-$(IPSEC_TOOLS_VER)\*.patch
-	touch $(IPSEC_TOOLS_DIR)/.source
+	toolchain/patch-kernel.sh $(IPSEC_TOOLS_DIR) package/ipsec-tools ipsec-tools-$(IPSEC_TOOLS_VERSION)\*.patch
+	touch $@
 
-$(IPSEC_TOOLS_DIR)/.configured: $(IPSEC_TOOLS_DIR)/.source
-	( cd $(IPSEC_TOOLS_DIR); \
-	  $(TARGET_CONFIGURE_OPTS) \
+$(IPSEC_TOOLS_DIR)/.configured: $(IPSEC_TOOLS_DIR)/.patched
+	( cd $(IPSEC_TOOLS_DIR); rm -rf config.cache; \
+		$(TARGET_CONFIGURE_OPTS) \
+		$(TARGET_CONFIGURE_ARGS) \
 	  CFLAGS="$(TARGET_CFLAGS)" \
-	  ./configure \
+	  LDFLAGS="$(TARGET_LDFLAGS)" \
+	  ./configure $(QUIET) \
 	  --target=$(GNU_TARGET_NAME) \
 	  --host=$(GNU_TARGET_NAME) \
 	  --build=$(GNU_HOST_NAME) \
@@ -82,75 +93,75 @@ $(IPSEC_TOOLS_DIR)/.configured: $(IPSEC_TOOLS_DIR)/.source
 	  --disable-hybrid \
 	  --without-libpam \
 	  --disable-gssapi \
-	  --with-kernel-headers=$(STAGING_DIR)/include \
+	  --enable-shared \
+	  --enable-static \
+	  --with-kernel-headers=$(LINUX_HEADERS_DIR) \
 	  $(IPSEC_TOOLS_CONFIG_FLAGS) \
-	); 
-	touch $(IPSEC_TOOLS_DIR)/.configured
+	)
+	# simpler than patching that cruft..
+	(echo '#undef bzero'; \
+	 echo '#define bzero(a, b) memset((a), 0, (b))'; \
+	 echo '#undef bcopy'; \
+	 echo '#define bcopy(src, dest, len) memmove(dest, src, len)'; \
+	 echo '#undef index'; \
+	 echo '#define index(a, b) strchr(a, b)'; \
+	) >> $(IPSEC_TOOLS_DIR)/config.h
+	touch $@
 
 $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_SETKEY) \
 $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_RACOON) \
 $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_RACOONCTL): \
     $(IPSEC_TOOLS_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(IPSEC_TOOLS_DIR)
-	
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(IPSEC_TOOLS_DIR)
+
 $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_SETKEY) \
 $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOON) \
 $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL): \
   $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_SETKEY) \
   $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_RACOON) \
-  $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_RACOONCTL) 
+  $(IPSEC_TOOLS_DIR)/$(IPSEC_TOOLS_BINARY_RACOONCTL)
 	$(MAKE) -C $(IPSEC_TOOLS_DIR) DESTDIR=$(TARGET_DIR) install
 	$(STRIP) --strip-unneeded --remove-section=.comment \
-          --remove-section=.note \
+	  --remove-section=.note \
 	  $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_SETKEY) \
 	  $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOON) \
 	  $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL)
-	-rm -f $(TARGET_DIR)/usr/man/man3/ipsec_strerror.3 \
-	  $(TARGET_DIR)/usr/man/man3/ipsec_set_policy.3 \
-	  $(TARGET_DIR)/usr/man/man5/racoon.conf.5 \
-	  $(TARGET_DIR)/usr/man/man8/racoonctl.8 \
-	  $(TARGET_DIR)/usr/man/man8/racoon.8 \
-	  $(TARGET_DIR)/usr/man/man8/plainrsa-gen.8 \
-	  $(TARGET_DIR)/usr/man/man8/setkey.8
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_LIBS)), y)
-	install -D $(IPSEC_TOOLS_DIR)/src/libipsec/.libs/libipsec.a \
-	  $(IPSEC_TOOLS_DIR)/src/libipsec/.libs/libipsec.la \
-	  $(IPSEC_TOOLS_DIR)/src/racoon/.libs/libracoon.a \
-	  $(IPSEC_TOOLS_DIR)/src/racoon/.libs/libracoon.la \
-	  $(STAGING_DIR)/lib
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_LIBS), y)
+	install -D $(addprefix $(IPSEC_TOOLS_DIR)/src/, \
+		libipsec/.libs/libipsec.a libipsec/.libs/libipsec.la \
+		racoon/.libs/libracoon.a racoon/.libs/libracoon.la) \
+		$(STAGING_DIR)/lib
 endif
-ifneq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT)), y)
-	-rm -f $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL)
+ifneq ($(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT), y)
+	rm -f $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL)
 endif
 
 IPSEC_TOOLS_PROGS= $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_SETKEY) \
     $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOON)
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT)), y)
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_ADMINPORT), y)
 IPSEC_TOOLS_PROGS+= $(TARGET_DIR)/$(IPSEC_TOOLS_TARGET_BINARY_RACOONCTL)
 endif
 
-ipsec-tools: uclibc openssl $(IPSEC_TOOLS_PROGS)
+#ipsec-tools: openssl flex host-flex $(IPSEC_TOOLS_PROGS)
+ipsec-tools: openssl flex $(IPSEC_TOOLS_PROGS)
 
 ipsec-tools-source: $(DL_DIR)/$(IPSEC_TOOLS_SOURCE)
 
 ipsec-tools-uninstall:
 
 ipsec-tools-clean:
-	$(MAKE) -C $(IPSEC_TOOLS_DIR) DESTDIR=$(TARGET_DIR) uninstall
-	$(MAKE) -C $(IPSEC_TOOLS_DIR) clean
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS_LIBS)), y)
-	-rm -f $(STAGING_DIR)/lib/libipsec.a
-	-rm -f $(STAGING_DIR)/lib/libipsec.la
-	-rm -f $(STAGING_DIR)/lib/libracoon.a
-	-rm -f $(STAGING_DIR)/lib/libracoon.la
+	-$(MAKE) -C $(IPSEC_TOOLS_DIR) DESTDIR=$(TARGET_DIR) uninstall
+	-$(MAKE) -C $(IPSEC_TOOLS_DIR) clean
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS_LIBS),y)
+	rm -f $(addprefix $(STAGING_DIR)/lib/, \
+		libipsec.a libipsec.la libracoon.a libracoon.la)
 endif
-	-rm $(IPSEC_TOOLS_DIR)/.configured
+	rm -f $(IPSEC_TOOLS_DIR)/.configured
 
 ipsec-tools-dirclean:
-	@echo $(LINUX_DIR)
-	-rm -rf $(IPSEC_TOOLS_DIR)
+	rm -rf $(IPSEC_TOOLS_DIR)
 
-ifeq ($(strip $(BR2_PACKAGE_IPSEC_TOOLS)), y)
+ifeq ($(BR2_PACKAGE_IPSEC_TOOLS), y)
 TARGETS+=ipsec-tools
-endif 
+endif
